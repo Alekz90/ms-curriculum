6b44d25e4b089c9442d9deb4dd885c6599c5497c
Alekz <alejdaf@outlook.com>
2025-11-03 22:14:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ms-curriculum
+Microservice for resumes creation
---
+++ src/main/java/alkz/mscurriculum/MsCurriculumApplication.java
@@ -0,0 +1,13 @@
+package alkz.mscurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurriculumApplication.class, args);
+	}
+
+}
